9e8ba2bdca052df4f7ec7f3e2884bf046c192244
Yacoubou-seidou <yacoseidou@gmail.com>
2024-04-16 10:02:38 +0100

    Login view

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,39 +1,85 @@
-import Link from 'next/link'
 import React from 'react'
-import Image from 'next/image'
-import mySvg from './svg'
-
+import './login.css'
+import logo from '../../../assets/images/logo-no-background.png'
+import Image from 'next/image';
 const Login = () => {
   return (
-        <>
-        <section className='login'>
-        <div className='container'>
-          <div className='backImg'>
-  {/* <Image src={BackgroundImage} className='svgBack' alt='background-image' /> */}
-  <mySvg/>
-            <div className='text'>
-              <h3>Login</h3>
-              <h1>My account</h1>
-            </div>
-          </div>
+    <div className="w-full contain">
 
-          <form >
-            <span>Username or email address *</span>
-            <input type='text' required  />
-            <span>Password *</span>
-            <input type='password' required  />
-            <button className='button' type='submit'>
-              Log in
-            </button>
-
-            <Link href='/register' className='link'>
-              Register
-            </Link>
-          </form>
+<div className="user_card flex w-2/6 flex-col justify-center px-6 py-12 lg:px-8">
+  <div className="sm:mx-auto sm:w-full sm:max-w-sm py-4">
+      <Image
+        src={logo}
+        className="mx-auto h-10 w-auto"
+        alt="Logo"
+      />
+    <h2 className="mt-10 text-center text-2xl leading-9 tracking-tight text-light-900">
+      Sign in to your account
+    </h2>
+  </div>
+  <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form className="space-y-6 w-full my-5" action="#" method="POST">
+      <div>
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Email address
+        </label>
+        <div className="mt-2">
+          <input
+            id="email"
+            name="email"
+            type="email"
+            autoComplete="email"
+            required=""
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
         </div>
-      </section>
-    </>
+      </div>
+      <div>
+        <div className="flex items-center justify-between">
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Password
+          </label>
+          <div className="text-sm">
+            <a
+              href="#"
+              className="font-semibold text-indigo-600 hover:text-indigo-500"
+            >
+              Forgot password?
+            </a>
+          </div>
+        </div>
+        <div className="mt-2">
+          <input
+            id="password"
+            name="password"
+            type="password"
+            autoComplete="current-password"
+            required=""
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+      <div>
+        <button
+          type="submit"
+          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Sign in
+        </button>
+      </div>
+    </form>
+  </div>
+</div>
+
+    </div>
   )
 }
 
 export default Login
+
