e90f1170dacacf43895070b4aceffcda85939e21
Yacoubou-seidou <yacoseidou@gmail.com>
2024-05-08 09:31:26 +0100

    Update context

--- src/app/context/Context.js
+++ src/app/context/Context.js
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useReducer } from "react"
-import Reducer from "./Reducer"
+import Reducer from "./Reducer.js"
 
 const initState = {
   user: JSON.parse(localStorage.getItem("user")) || null,
